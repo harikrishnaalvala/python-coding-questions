1de3dc24fcae82753f7e8c9a0928a32e0cd6a5ac
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-06-24 16:55:51 +0530

    Create sum_non_prime.py

---
+++ sum_non_prime.py
@@ -0,0 +1,25 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def sum_non_prime(numbers):
+    non_prime_sum = 0
+    for num in numbers:
+        if not is_prime(num):
+            non_prime_sum += num
+    return non_prime_sum
+
+# Test the function with the example
+input_array = [4, 2, 5, 7, 1]
+output = sum_non_prime(input_array)
+print("Output:", output) # 5
