bdfc4e58fe2c4150df38443e3ad5bcafd12839a5
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-06-27 10:45:40 +0530

    Create add_lists.py

---
+++ add_lists.py
@@ -0,0 +1,8 @@
+def add_lists(list1, list2):
+    return list1 + list2
+
+# Test the function with the example
+input_list1 = [3, 324, 64]
+input_list2 = [2, 7, 1, 6]
+output = add_lists(input_list1, input_list2)
+print("Output:", output) # Output: [3,324,64,2,7,1,6]
