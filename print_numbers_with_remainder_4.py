7e4640a0f2068680751cb0865c4930d181947f77
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-01 23:43:19 +0530

    Refactor test output for print_numbers_with_remainder_4

--- print_numbers_with_remainder_4.py
+++ print_numbers_with_remainder_4.py
@@ -7,8 +7,7 @@ def print_numbers_with_remainder_4(arr):
 input_arr1 = [19, 10, 44, 3, 11, 129]
 input_arr2 = [13, 4]
 
-print("Output 1:")
-print_numbers_with_remainder_4(input_arr1)
 
-print("\\nOutput 2:")
-print_numbers_with_remainder_4(input_arr2)
+print_numbers_with_remainder_4(input_arr1) # 19 44 129 
+
+print_numbers_with_remainder_4(input_arr2) # 4
