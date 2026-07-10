d815f0f5c6c54ca145f9c00c37911cca0c6afefd
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-10 23:07:17 +0530

    Update print_numbers_divisible_by_11.py

--- print_numbers_divisible_by_11.py
+++ print_numbers_divisible_by_11.py
@@ -1,10 +1,10 @@
 def print_numbers_divisible_by_11(start, end):
-    print("Numbers divisible by 11:")
+    
     for num in range(start, end + 1):
         if num % 11 == 0:
-            print(num)
+            print(num,end=" ")
 
 # Test case
 start_num = 1
 end_num = 100
-print_numbers_divisible_by_11(start_num, end_num)
+print_numbers_divisible_by_11(start_num, end_num) # 11 22 33 44 55 66 77 88 99 
