ca0c55b3285b7235f3c10cf0b1bb5df57c32ddb1
Harikrishna Alwala <130495126+harikrishnaalvala@users.noreply.github.com>
2026-07-15 16:44:12 +0530

    Update sum_divisible_by_3_and_5.py

--- sum_divisible_by_3_and_5.py
+++ sum_divisible_by_3_and_5.py
@@ -11,4 +11,4 @@ def sum_divisible_by_3_and_5():
 
 # Calculate and print the sum
 result = sum_divisible_by_3_and_5()
-print("Sum of numbers divisible by both 3 and 5 between 1 and 1000:", result)
+print("Sum of numbers divisible by both 3 and 5 between 1 and 1000:", result) # Sum of numbers divisible by both 3 and 5 between 1 and 1000: 33165
